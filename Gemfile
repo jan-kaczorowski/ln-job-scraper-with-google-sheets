2efe08e53d4ff9049d6be708a7ef3eeade3c692c
Jan Kaczorowski <jan.kaczorowski@gmail.com>
2025-06-12 15:48:30 +0200

    remove

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'capybara', '~> 3.39'
-gem 'selenium-webdriver', '~> 4.15'
-gem 'nokogiri', '~> 1.15'
--- setup_node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing dependencies..."
-npm install puppeteer turndown
-
-echo "Setup complete! Run with:"
-echo "node linkedin_scraper.js \"your keywords\""
